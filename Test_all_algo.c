888037246600bd272715c6fe42209688185be6d7
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-03-10 16:31:32 +0100

    Update Test_all_algo.c

--- Test_all_algo.c
+++ Test_all_algo.c
@@ -7,6 +7,7 @@ void main(void){
      x  = FIRST_ALGO_H_next(x);
      printf("%d \n", x);
   }
+  int x = 473;
   for (int i = 0; i < 100;i++){
      x  = SECOND_ALGO_H_next(x);
      printf("%d \n", x);
