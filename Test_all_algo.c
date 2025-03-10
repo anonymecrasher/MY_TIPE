35c9eaa6e46bdd665e665c1235868a41997e4bd9
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-03-10 16:30:49 +0100

    Update Test_all_algo.c

--- Test_all_algo.c
+++ Test_all_algo.c
@@ -1,9 +1,14 @@
 #include "first_algo.h"
+#include "second_algo.h"
 
 void main(void){
   int x = 473;
   for (int i = 0; i < 100;i++){
      x  = FIRST_ALGO_H_next(x);
+     printf("%d \n", x);
+  }
+  for (int i = 0; i < 100;i++){
+     x  = SECOND_ALGO_H_next(x);
      printf("%d \n", x);
   }
 }
