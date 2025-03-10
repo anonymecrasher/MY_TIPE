90ab1c20addb4ddb59f5056acc5730629db3764a
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-03-10 16:28:54 +0100

    Update and rename second_methode.h to second_algo.h

---
+++ second_algo.h
@@ -0,0 +1,7 @@
+#ifndef SECOND_ALGO_H
+#define SECOND_ALGO_H
+
+int SECOND_ALGO_H_next(int x);
+
+
+#endif
--- second_methode.h
+++
@@ -1,7 +0,0 @@
-#ifndef SECOND_METHODE_H
-#define SECOND_METHODE_H
-
-int SECOND_METHODE_H(int x);
-
-
-#endif
