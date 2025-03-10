b4544c31d1b1f53d0fd2ea06690dc356bae9f567
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-03-10 16:12:42 +0100

    Update first_algo.c

--- first_algo.c
+++ first_algo.c
@@ -4,7 +4,7 @@ int FIRST_ALGO_H_carre(int x){
    return x * x;
 }
 int FIRST_ALGO_H_cut(int x){
-  return ( x / 10 ) % 100;
+  return ( x / 10 ) % 1000;
 }
 int FIRST_ALGO_H_next(int x){
   return FIRST_ALGO_H_cut(FIRST_ALGO_H_carre(x));
