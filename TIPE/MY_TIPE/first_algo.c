72911dec559f230f2b433f2e1004dd9539e00004
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-03-31 15:54:43 +0200

    Update first_algo.c

--- TIPE/MY_TIPE/first_algo.c
+++ TIPE/MY_TIPE/first_algo.c
@@ -1,11 +1,19 @@
 #include "first_algo.h"
 
-int FIRST_ALGO_H_carre(int x){
-   return x * x;
+int FIRST_ALGO_H_pow(int x,int n){
+   if (n == 0) return 1;
+   if (n == 1) return x;
+   int t = FIRST_ALGO_H_pow(x, n/2);
+   else if (n % 2 == 0) 
+      return t * t;
+   else if (n % 2 == 1)
+      return t * t * x
 }
 int FIRST_ALGO_H_cut(int x){
   return ( x / 10 ) % 1000;
 }
-int FIRST_ALGO_H_next(int x){
-  return FIRST_ALGO_H_cut(FIRST_ALGO_H_carre(x));
+int FIRST_ALGO_H_next(int x,int n){
+   return FIRST_ALGO_H_cut(
+     FIRST_ALGO_H_pow(33 * n, 4 * n) * FIRST_ALGO_H_pow(x, n) +
+     FIRST_ALGO_H_next(x, n-1));
 }
