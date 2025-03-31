a8987ced6143113d92bee25b3ceb405375c8119e
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-03-31 15:55:57 +0200

    Update first_algo.c

--- TIPE/MY_TIPE/first_algo.c
+++ TIPE/MY_TIPE/first_algo.c
@@ -13,6 +13,7 @@ int FIRST_ALGO_H_cut(int x){
   return ( x / 10 ) % 1000;
 }
 int FIRST_ALGO_H_next(int x,int n){
+   if (n == 0) return 57;
    return FIRST_ALGO_H_cut(
      FIRST_ALGO_H_pow(33 * n, 4 * n) * FIRST_ALGO_H_pow(x, n) +
      FIRST_ALGO_H_next(x, n-1));
