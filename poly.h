27c71bc006368748a3d796c918f7c98f572d00ec
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-10-06 16:06:29 +0200

    Update poly.h

--- poly.h
+++ poly.h
@@ -1,1 +1,14 @@
+#ifndef POLY_H
+#define POLY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// evaluation of the polynom in x
+int eval(int *p, int n, int x);
+
+
+
+
+#endif POLY_H
