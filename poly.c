b9307d5f2669eadd4d27a52c11d6d48816f97082
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-10-06 16:29:20 +0200

    Update poly.c

--- poly.c
+++ poly.c
@@ -1,5 +1,12 @@
 #import 'poly.h'
 
+int **init_all_p(void){// 73 73 73 73 73 73 73 73 73 73 73 73 73 73
+  int **p = malloc(73 * sizeof(int*));
+  for (int i = 0; i < 73; i++){
+    p = malloc(219 * sizeof(int));
+  }
+
+
 int eval(int *p, int n, int x) {
   int ret = p[n]
   for (int i = n; i > 0; i--) {
