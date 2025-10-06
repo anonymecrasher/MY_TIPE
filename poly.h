dd30b4fe8aa916dfc4ebaa7636342845b2ac4eed
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-10-06 16:18:55 +0200

    Update poly.h

--- poly.h
+++ poly.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+// initialisation de tout les polynome
+int **init_all_p(void);
+
+
 // evaluation of the polynom in x
 int eval(int *p, int n, int x);
 
